19fb1f0422de9349ecb2c73624876e07c5385607
ddofka <ddorofejevas@yahoo.com>
2025-03-26 18:54:24 +0200

    Initial commit

---
+++ src/main/java/org/codeacademy/siuntupristatymas/SiuntuPristatymasApplication.java
@@ -0,0 +1,13 @@
+package org.codeacademy.siuntupristatymas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiuntuPristatymasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SiuntuPristatymasApplication.class, args);
+    }
+
+}
